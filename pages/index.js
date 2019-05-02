97e39e7ddbefc9ca45d4facd071b95f401eb4a31
Tatiana Uspenskaia <tanyauspenskaya@gmail.com>
2019-05-02 13:47:12 -0400

    Add index page and npm scripts

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// Next.js takes care of React imports and exports, which means you don’t need to import React components, and exports should always be set to default.
+// WOW!!!
+
+const Index = () => (
+  <div>
+    <p>Shopify Sample App</p>
+  </div>
+);
+
+export default Index;
