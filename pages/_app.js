185dddc847ca059aea6dfef63fbea6620833f644
Tatiana Uspenskaia <tanyauspenskaya@gmail.com>
2019-05-03 16:27:48 -0400

    Add EmptyState and Annotated Layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,13 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import '@shopify/polaris/styles.css';
+import Cookies from 'js-cookie';
 
 class EmbeddedApp extends App {
+  state = {
+    shopOrigin: Cookies.get('shopOrigin'),
+  };
+
   render() {
     const { Component, pageProps } = this.props;
     return(
@@ -12,7 +17,11 @@ class EmbeddedApp extends App {
           <title>Shopify Embedded App</title>
           <meta charSet="utf-8" />
         </Head>
-        <AppProvider>
+        <AppProvider
+          apiKey={API_KEY}
+          shopOrigin={this.state.shopOrigin}
+          forceRedirect
+        >
           <Component {...pageProps} />
         </AppProvider>
       </React.Fragment>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
-import { TextStyle } from '@shopify/polaris';
+import { 
+  Page,
+  Layout,
+  EmptyState,
+} from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg'
 
 const Index = () => (
-  <div>
-    <TextStyle variation="positive">
-      Shopify Sample App
-    </TextStyle>
-  </div>
+  <Page
+    primaryAction={{
+      content: 'Select Products'
+    }}
+  >
+    <Layout>
+      <EmptyState 
+        heading="Discount your products temporarily"
+        action={{
+          content: 'Select products',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
+      </EmptyState>
+    </Layout>
+  </Page>
 );
 
 export default Index;
